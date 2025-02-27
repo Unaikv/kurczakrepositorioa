cbba2ed8657bf009839b2b9a281e936a106cb2ef
Unaikv <156773569+Unaikv@users.noreply.github.com>
2025-02-27 23:29:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -38,3 +38,37 @@ function beltza2(){
     document.body.style.background = "black";
 }
 
+function tamaina1() {
+    document.getElementsByTagName("h1")[0].style.fontSize = "x-small";
+    document.getElementsByTagName("h2")[0].style.fontSize = "x-small";
+    document.getElementsByTagName("h3")[0].style.fontSize = "x-small";
+}
+
+function tamaina2() {
+    document.getElementsByTagName("h1")[0].style.fontSize = "small";
+    document.getElementsByTagName("h2")[0].style.fontSize = "small";
+    document.getElementsByTagName("h3")[0].style.fontSize = "small";
+}
+
+function tamaina3() {
+    document.getElementsByTagName("h1")[0].style.fontSize = "medium";
+    document.getElementsByTagName("h2")[0].style.fontSize = "medium";
+    document.getElementsByTagName("h3")[0].style.fontSize = "medium";
+}
+
+function tamaina4() {
+    document.getElementsByTagName("h1")[0].style.fontSize = "large";
+    document.getElementsByTagName("h2")[0].style.fontSize = "large";
+    document.getElementsByTagName("h3")[0].style.fontSize = "large";
+}
+
+function tamaina5() {
+    document.getElementsByTagName("h1")[0].style.fontSize = "x-large";
+    document.getElementsByTagName("h2")[0].style.fontSize = "x-large";
+    document.getElementsByTagName("h3")[0].style.fontSize = "x-large";
+    
+}
+
+function estiloa(fuente) {
+    document.getElementById("testua").style.fontFamily = fuente;
+}
