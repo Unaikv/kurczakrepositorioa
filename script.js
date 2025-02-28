aa0fff26348a9c72f91934ce8725ce77b400de1d
Unaikv <156773569+Unaikv@users.noreply.github.com>
2025-02-28 18:51:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,74 +1,15 @@
-function gorria(){
-    document.getElementById("testua").style.color = "red";
+function konprobaketa(event) {
+    event.preventDefault();
+    const emaila = document.getElementById("emaila").value;
+    const emaitza = document.getElementById("testua");
+
+    if (emaila.includes("@"))  {
+        emaitza.textContent = "Emaila Egokia da!";
+        emaitza.style.color = "green";
+    } else {
+        emaitza.textContent = "Errorea: Emailak '@' izan behar du.";
+        emaitza.style.color = "red";
+    }
 }
 
-function berdea(){
-    document.getElementById("testua").style.color = "green";
-}
-
-function urdina(){
-    document.getElementById("testua").style.color = "blue";
-}
-
-function txuria(){
-    document.getElementById("testua").style.color = "white";
-}
-
-function beltza(){
-    document.getElementById("testua").style.color = "black";
-}
-
-function gorria2(){
-    document.body.style.background = "red";
-}
-
-function berdea2(){
-    document.body.style.background = "green";
-}
-
-function urdina2(){
-    document.body.style.background = "blue";
-}
-
-function txuria2(){
-    document.body.style.background = "white";
-}
-
-function beltza2(){
-    document.body.style.background = "black";
-}
-
-function tamaina1() {
-    document.getElementsByTagName("h1")[0].style.fontSize = "x-small";
-    document.getElementsByTagName("h2")[0].style.fontSize = "x-small";
-    document.getElementsByTagName("h3")[0].style.fontSize = "x-small";
-}
-
-function tamaina2() {
-    document.getElementsByTagName("h1")[0].style.fontSize = "small";
-    document.getElementsByTagName("h2")[0].style.fontSize = "small";
-    document.getElementsByTagName("h3")[0].style.fontSize = "small";
-}
-
-function tamaina3() {
-    document.getElementsByTagName("h1")[0].style.fontSize = "medium";
-    document.getElementsByTagName("h2")[0].style.fontSize = "medium";
-    document.getElementsByTagName("h3")[0].style.fontSize = "medium";
-}
-
-function tamaina4() {
-    document.getElementsByTagName("h1")[0].style.fontSize = "large";
-    document.getElementsByTagName("h2")[0].style.fontSize = "large";
-    document.getElementsByTagName("h3")[0].style.fontSize = "large";
-}
-
-function tamaina5() {
-    document.getElementsByTagName("h1")[0].style.fontSize = "x-large";
-    document.getElementsByTagName("h2")[0].style.fontSize = "x-large";
-    document.getElementsByTagName("h3")[0].style.fontSize = "x-large";
-    
-}
-
-function estiloa(fuente) {
-    document.getElementById("testua").style.fontFamily = fuente;
-}
+document.getElementById("konprobaketa").addEventListener("click", konprobaketa);
--- script2.js
+++ script2.js
@@ -1,19 +1,74 @@
-    function zenbakia(n){
-    return(Math.floor(Math.random() * n + 1 ));
+function gorria(){
+    document.getElementById("testua").style.color = "red";
 }
 
-var irudia = new Array();
-irudia[0] = "irudiak/blog.png";
-irudia[1] = "irudiak/css_logo.png";
-irudia[2] = "irudiak/html_logo.png";
-irudia[3] = "irudiak/js_logo.png";
-
-
-function aukeratu_irudia(){
-    document.getElementById("aukeratuta").src = irudia[zenbakia(4)-1];
+function berdea(){
+    document.getElementById("testua").style.color = "green";
 }
 
-function loop() {
-    document.getElementById("argazkia").src = irudia[zenbakia(4)-1];
-    setTimeout(loop, 100);
+function urdina(){
+    document.getElementById("testua").style.color = "blue";
+}
+
+function txuria(){
+    document.getElementById("testua").style.color = "white";
+}
+
+function beltza(){
+    document.getElementById("testua").style.color = "black";
+}
+
+function gorria2(){
+    document.body.style.background = "red";
+}
+
+function berdea2(){
+    document.body.style.background = "green";
+}
+
+function urdina2(){
+    document.body.style.background = "blue";
+}
+
+function txuria2(){
+    document.body.style.background = "white";
+}
+
+function beltza2(){
+    document.body.style.background = "black";
+}
+
+function tamaina1() {
+    document.getElementsByTagName("h1")[0].style.fontSize = "x-small";
+    document.getElementsByTagName("h2")[0].style.fontSize = "x-small";
+    document.getElementsByTagName("h3")[0].style.fontSize = "x-small";
+}
+
+function tamaina2() {
+    document.getElementsByTagName("h1")[0].style.fontSize = "small";
+    document.getElementsByTagName("h2")[0].style.fontSize = "small";
+    document.getElementsByTagName("h3")[0].style.fontSize = "small";
+}
+
+function tamaina3() {
+    document.getElementsByTagName("h1")[0].style.fontSize = "medium";
+    document.getElementsByTagName("h2")[0].style.fontSize = "medium";
+    document.getElementsByTagName("h3")[0].style.fontSize = "medium";
+}
+
+function tamaina4() {
+    document.getElementsByTagName("h1")[0].style.fontSize = "large";
+    document.getElementsByTagName("h2")[0].style.fontSize = "large";
+    document.getElementsByTagName("h3")[0].style.fontSize = "large";
+}
+
+function tamaina5() {
+    document.getElementsByTagName("h1")[0].style.fontSize = "x-large";
+    document.getElementsByTagName("h2")[0].style.fontSize = "x-large";
+    document.getElementsByTagName("h3")[0].style.fontSize = "x-large";
+    
+}
+
+function estiloa(fuente) {
+    document.getElementById("testua").style.fontFamily = fuente;
 }
